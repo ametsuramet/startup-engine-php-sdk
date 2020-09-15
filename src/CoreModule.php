a1c923c3935aeabfd0f8325e192413b645e6c994
Rahmat Supriatna <ametsuramet@gmail.com>
2020-09-15 08:53:14 +0700

    First init

---
+++ src/CoreModule.php
@@ -0,0 +1,57 @@
+<?php
+namespace Ametsuramet\StartupEngine;
+use GuzzleHttp;
+
+class PublicModule {
+    private String $baseUrl = "http://localhost:9000/api/v1";
+    private String $appKey;
+    private String $appSecret;
+    private String $feature;
+    private $client;
+    private $httpMethod = "GET";
+    private $query;
+    private $endpoint;
+
+    function __construct($appKey, $appSecret = "")
+    {
+        $this->appKey = $appKey;
+        $this->appSecret = $appSecret;
+        $this->client =  new GuzzleHttp\Client([
+            'base_uri' => $this->baseUrl,
+            'headers' => [
+                "APP-ID" => $appKey,
+            ]
+        ]);
+
+    }
+
+    public function setBaseUrl($baseUrl) {
+        $this->baseUrl = $baseUrl;
+    }
+
+    public function getList($feature,  $search = "", $page = 1, $limit = 20) 
+    {
+        $this->httpMethod = "GET";
+        $this->query = [
+            "type" => $feature,
+            "search" => $search,
+            "page" => $page,
+            "limit" => $limit,
+        ];
+        
+        $this->endpoint = "/public/feature";
+
+        return $this->send();
+    }
+
+    private function send() 
+    {
+        $res = $this->client->request($this->httpMethod, $this->endpoint, [
+            'query' => $this->query,
+        ]);
+        if  ($res->getStatusCode() != 200) {
+            throw new \Exception("Error Request => " . $res->getStatusCode());
+        }
+        return $res->getBody();
+    }
+}
