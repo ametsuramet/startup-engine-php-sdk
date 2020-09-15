dc12d1ffb94f1ac2c86fed0906d2a40504a7209d
Rahmat Supriatna <ametsuramet@gmail.com>
2020-09-15 09:44:25 +0700

    add core auth

---
+++ src/CoreAuth.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Ametsuramet\StartupEngine;
+
+use GuzzleHttp;
+
+
+class CoreAuth
+{
+    private String $baseUrl = "http://localhost:9000";
+    private String $username;
+    private String $password;
+    private String $device;
+    private String $fcm_token;
+    private String $httpMethod = "POST";
+    private String $body;
+
+    function __construct()
+    {
+    }
+
+    public function login($username, $password, $device, $fcm_token)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->device = $device;
+        $this->fcm_token = $fcm_token;
+        $this->body = json_encode([
+            "username" => $username,
+            "password" => $password,
+            "device" => $device,
+            "fcm_token" => $fcm_token,
+        ]);
+        $this->setClient();
+        $this->send();
+    }
+
+    public function setBaseUrl($baseUrl)
+    {
+        $this->baseUrl = $baseUrl;
+    }
+
+    private function setClient()
+    {
+        $this->client =  new GuzzleHttp\Client([
+            'base_uri' => $this->baseUrl,
+            'headers' => [
+                "APP-ID" => $this->appKey,
+            ]
+        ]);
+    }
+
+
+    private function send()
+    {
+        $res = $this->client->request($this->httpMethod, $this->endpoint, [
+            'query' => $this->query,
+            'body' => $this->body,
+        ]);
+        if ($res->getStatusCode() != 200) {
+            throw new \Exception("Error Request => " . $res->getStatusCode());
+        }
+        return json_decode($res->getBody()->getContents(), true);
+    }
+}
